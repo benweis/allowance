fbd2533591d590481ea174e064fbc051e76dba0b
benweis <weisben@gmail.com>
2014-08-24 14:58:33 -0700

    003
    basic list foundation

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,48 +1,39 @@
-angular.module('starter.controllers', [])
+angular.module('ionicApp', ['ionic'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
+.controller('MyCtrl', function($scope) {
+  
+  $scope.data = {
+    showDelete: false
   };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
+  
+  $scope.edit = function(item) {
+    alert('Edit Item: ' + item.id);
   };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+  $scope.share = function(item) {
+    alert('Share Item: ' + item.id);
   };
-})
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+  
+  $scope.moveItem = function(item, fromIndex, toIndex) {
+    $scope.items.splice(fromIndex, 1);
+    $scope.items.splice(toIndex, 0, item);
+  };
+  
+  $scope.onItemDelete = function(item) {
+    $scope.items.splice($scope.items.indexOf(item), 1);
+  };
+  
+  $scope.items = [
+    { id: 0 },
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 },
+    { id: 10 },
   ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+  
 });
